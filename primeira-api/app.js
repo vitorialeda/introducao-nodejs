ece54bf18cf5648003fb1d7e134804e315e054bc
vitorialeda <vitoria_lnogueira@hotmail.com>
2025-03-30 14:55:03 -0300

    Introdução a Middlewares no Express.js

--- primeira-api/app.js
+++ primeira-api/app.js
@@ -1,15 +1,21 @@
-import http from 'http';
 import sqlite3 from 'sqlite3'; 
 import express from 'express';
 import { sequelize } from './models.js';
-import rotas from './routes.js';
 
 const app = express();
 
 app.use((req, res, next)=>{
     console.log('Digite 9 para falar com um atendente');
+    next();
 });
 
+app.use((req, res, next)=>{
+    res.send({ mensagem: 'Problema resolvido' });
+});
+
+app.use((req, res, next)=>{
+    console.log('Segue o link para baixar o driver atualizado');
+});
 
 async function inicializaApp(){
     const db = new sqlite3.Database('./tic.db', (erro) => {
@@ -23,14 +29,9 @@ async function inicializaApp(){
     
     await sequelize.sync();
 
-    const servidor = http.createServer(app);
-    
     const porta = 3000;
-    const host = 'localhost';
     
-    servidor.listen(porta, host, () => {
-        console.log(`Servidor executando em http://${host}:${porta}/`);
-     });
+    app.listen(porta);
 }
 
 inicializaApp();
